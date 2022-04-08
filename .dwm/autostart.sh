1fe7e83979fc2105b05c8d26eb89dae47b012628
Mina Matta <Minamatta98@gmail.com>
2022-04-08 23:01:21 +1000

    scripts modified

---
+++ .dwm/autostart.sh
@@ -0,0 +1,10 @@
+numlockx on &
+$HOME/.config/feh/.fehbg &
+picom &
+xbindkeys &
+xset r rate 300 55
+#export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/home/mina/.emacs.d/bin:/Storage/dwmscripts/
+dwmblocks &
+clipmenud &
+kdeconnect-cli &
+
